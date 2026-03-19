7966bfd163a064ab32943eeb884f37e735cf992e
tejasbarge <150351382+tejasbarge@users.noreply.github.com>
2026-03-19 21:31:19 +0530

    Time: 16 ms (90.66%), Space: 48.1 MB (79.64%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.java
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.java
@@ -1,21 +1,36 @@
 class Solution {
     public int findLHS(int[] nums) {
-        Arrays.sort(nums);
-        int left=0,right=0,maxi=0;
-        int prev=nums[0];
-        while(right<nums.length){
-            int curr=nums[right];
-            int diff=Math.abs(nums[right]-nums[left]);
-            if(diff==1){
-                maxi=right-left+1>maxi?right-left+1:maxi;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int num:nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        int maxi=0;
+        for(int key: map.keySet()){
+            if(map.containsKey(key+1)){
+                int length=map.get(key)+map.get(key+1);
+                maxi=Math.max(maxi,length);
             }
-            else{
-                while(left<right && Math.abs(nums[left]-nums[right])>1){
-                    left++;
-                }
-            }
-            right++;
         }
         return maxi;
     }
 }
+
+
+//using sliding window+sorting
+// Arrays.sort(nums);
+//         int left=0,right=0,maxi=0;
+//         int prev=nums[0];
+//         while(right<nums.length){
+//             int curr=nums[right];
+//             int diff=Math.abs(nums[right]-nums[left]);
+//             if(diff==1){
+//                 maxi=right-left+1>maxi?right-left+1:maxi;
+//             }
+//             //else{
+//                 while(left<right && Math.abs(nums[left]-nums[right])>1){
+//                     left++;
+//                 }
+//             //}
+//             right++;
+//         }
+//         return maxi;
