ecfab16d14ad9f8d9d27ec1ec6acbcf8d9fa4641
tejasbarge <150351382+tejasbarge@users.noreply.github.com>
2026-03-25 22:14:30 +0530

    Time: 5 ms (47.55%), Space: 159.2 MB (97.2%) - LeetHub

--- 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.java
+++ 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.java
@@ -2,42 +2,68 @@ class Solution {
     public boolean canPartitionGrid(int[][] grid) {
         int rows=grid.length;
         int cols=grid[0].length;
-        long[] rowsum=new long[rows];
-        long[] colsum=new long[cols];
-        long totalrowsum=0;
+        long total=0;
         for(int i=0;i<rows;i++){
-            int sum=0;
             for(int j=0;j<cols;j++){
-                sum+=grid[i][j];
-                totalrowsum+=grid[i][j];
+                total+=grid[i][j];
             }
-            rowsum[i]=sum;
         }
-        int c=0;
-        long totalcolsum=0;
-        while(c<cols){
-            int sum=0;
-            for(int i=0;i<rows;i++){
-                sum+=grid[i][c];
-                totalcolsum+=grid[i][c];
+
+        //for row
+        long currsumrow=0;
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                currsumrow+=grid[i][j];
             }
-            colsum[c]=sum;
-            c++;
-        }
-        if(check(rowsum,totalrowsum) || check(colsum,totalcolsum)){
-            return true;
-        }
-        return false;
-    }
-    static boolean check(long[] rowsum,long totalrowsum){
-        long currsum=0;
-        for(int i=0;i<rowsum.length-1;i++){
-            currsum+=rowsum[i];
-            totalrowsum-=rowsum[i];
-            if(currsum==totalrowsum){
+
+            if(currsumrow==total-currsumrow){
                 return true;
             }
         }
+
+        //for columns
+        long currsumcol=0;
+        for(int i=0;i<cols;i++){
+            for(int j=0;j<rows;j++){
+                currsumcol+=grid[j][i];
+            }
+            if(currsumcol==total-currsumcol){
+                return true;
+            }
+        } 
         return false;
     }
+   
 }
+
+
+//    long[] rowsum=new long[rows];
+//         long[] colsum=new long[cols];
+//         long totalrowsum=0;
+//         long totalcolsum=0;
+//         for(int i=0;i<rows;i++){
+//             for(int j=0;j<cols;j++){
+//                 int val=grid[i][j];
+//                 rowsum[i]+=val;
+//                 colsum[j]+=val;
+//                 totalrowsum+=grid[i][j];
+//                 totalcolsum+=grid[i][j];
+//             }
+//         }
+//         if(check(rowsum,totalrowsum) || check(colsum,totalcolsum)){
+//             return true;
+//         }
+//         return false;
+//     }
+
+//  static boolean check(long[] rowsum,long totalrowsum){
+//         long currsum=0;
+//         for(int i=0;i<rowsum.length-1;i++){
+//             currsum+=rowsum[i];
+//             totalrowsum-=rowsum[i];
+//             if(currsum==totalrowsum){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
