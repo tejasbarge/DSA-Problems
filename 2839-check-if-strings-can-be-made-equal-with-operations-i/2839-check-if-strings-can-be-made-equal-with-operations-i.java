cd4218fcedafe48a9847c14a9252e7c667a59ae0
tejasbarge <150351382+tejasbarge@users.noreply.github.com>
2026-03-29 10:53:22 +0530

    Time: 1 ms (100%), Space: 43.8 MB (96.43%) - LeetHub

--- 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.java
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.java
@@ -1,29 +1,30 @@
 class Solution {
     public boolean canBeEqual(String s1, String s2) {
-    //    if(s1.charAt(0)!=s2.charAt(0) || s1.charAt(0) !=s2.charAt(3)){
-    //     return false;
-    //    }
     boolean res1=check(0,2,s1,s2);
     boolean res2=check(1,3,s1,s2);
-    if(res1==true && res2==true){
-        return true;
-    }
-    return false;
+    return res1==true && res2==true;
         
     }
     static boolean check(int i,int j,String s1,String s2){
-        if(s1.charAt(i)==s2.charAt(i)){
-            if(s1.charAt(j)==s2.charAt(j)){
-                return true;
-            }
-            return false;
+        char a=s1.charAt(i), b=s1.charAt(j);
+        char c=s2.charAt(i), d=s2.charAt(j);
+        if((a==c && b==d) || (a==d && b==c)){
+            return true;
         }
-        else if(s1.charAt(i)==s2.charAt(j)){
-                if(s1.charAt(j)==s2.charAt(i)){
-                    return true;
-                }
-                return false;
-            }
         return false;
     }
 }
+
+// if(s1.charAt(i)==s2.charAt(i)){
+//             if(s1.charAt(j)==s2.charAt(j)){
+//                 return true;
+//             }
+//             return false;
+//         }
+//         else if(s1.charAt(i)==s2.charAt(j)){
+//                 if(s1.charAt(j)==s2.charAt(i)){
+//                     return true;
+//                 }
+//                 return false;
+//             }
+//         return false;
