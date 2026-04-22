b5c9a8289df9165185ae0d8919752d8b8a176d03
Tejas Barge <150351382+tejasbarge@users.noreply.github.com>
2026-04-22 20:17:39 +0530

    Time: 1 ms (100%), Space: 48 MB (43.4%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int xor=0;
+        for(int num:nums){
+            xor^=num;
+        }
+        int mask=xor & (-xor);
+        int[] ans=new int[2];
+        for(int num:nums){
+            if((num&mask)==0){
+                ans[0]^=num;;
+            }
+            else{
+                ans[1]^=num;
+            }
+        }
+        return ans;
+    }
+}
